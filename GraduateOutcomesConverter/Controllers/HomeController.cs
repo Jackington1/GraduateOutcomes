504b67ad69e8c69c36e6eb32d2b719a800397308
Jack Newman <jack.newman@uwe.ac.uk>
2019-09-11 15:06:56 +0100

    Updated XML format

--- GraduateOutcomesConverter/Controllers/HomeController.cs
+++ GraduateOutcomesConverter/Controllers/HomeController.cs
@@ -123,59 +123,109 @@ namespace GraduateOutcomesConverter.Controllers
             return Content(System.IO.File.ReadAllText(filepath));
         }
 
-
         public IActionResult Convert()
         {
             var webroot = _env.WebRootPath;
             var filepath = TempData["filepath"].ToString();
-            
+
             string[] reader = System.IO.File.ReadAllLines(Path.Combine(webroot, filepath));
-            XElement cust = new XElement("Root",
+            XElement Record = new XElement("Records",
                 from str in reader
                 let fields = str.Split(',')
-                select new XElement("Student",
-                new XAttribute("HUSID", fields[0]),
-                new XElement("OWNSTU", fields[1]),
-                new XElement("Country", fields[2]),
-                new XElement("GOForenames", fields[3]),
-                new XElement("GOSurname", fields[4]),
-                new XElement("IsisForename", fields[5]),
-                new XElement("IsisSurname", fields[6]),
-                new XElement("FNameChange", fields[7]),
-                new XElement("SNameChange", fields[8]),
-                new XElement("GradStatus", fields[9]),
-                new XElement("UKMob", fields[10]),
-                new XElement("UKMob2", fields[11]),
-                new XElement("UKTel", fields[12]),
-                new XElement("UKTel2", fields[13]),
-                new XElement("IntTel", fields[14]),
-                new XElement("InteTel2", fields[15]),
-                new XElement("Email", fields[16]),
-                new XElement("Email2", fields[17]),
-                new XElement("PersonUrn", fields[18]),
-                new XElement("TblMasterFacultyCode", fields[19]),
-                new XElement("PrimaryDept", fields[20]),
-                new XElement("StudentNumber", fields[21]),
-                new XElement("TblAddStuDetailsCode", fields[22]),
-                new XElement("PrimaryTargetCode", fields[23]),
-                new XElement("PrimaryTargetName", fields[24]),
-                new XElement("IncomeStatusCode", fields[25]),
-                new XElement("EnrolmentStatusCode", fields[26]),
-                new XElement("MobileNumber", fields[27]),
-                new XElement("PersonalEmail", fields[28]),
-                new XElement("EmailAddress", fields[29]),
-                new XElement("TypeCode", fields[30]),
-                new XElement("TelephoneNumber", fields[31]),
-                new XElement("HesaReasonForLeavingCode", fields[32]),
-                new XElement("DateOfWithdrawal", fields[33]),
-                new XElement("HFormattedTelNumber", fields[34]),
-                new XElement("MatchMobileAndTelNo", fields[35])
+                select new XElement("GraduateOutcomeRecord",
+                new XElement("Provider",
+                new XElement("RECID", "17071"),
+                new XElement("UKPRN", "10007164"),
+                new XElement("CENSUS", fields[0])),
+                new XElement("GRADUATE",
+                new XElement("HUSID", fields[1]),
+                new XElement("OWNSTU", fields[2]),
+                new XElement("COUNTRY", fields[3]),
+                new XElement("EMAIL", fields[4]),
+                new XElement("EMAIL2", fields[5]),
+                new XElement("EMAIL3", fields[6]),
+                new XElement("EMAIL4", fields[7]),
+                new XElement("EMAIL5", fields[8]),
+                new XElement("FNAMES", fields[9]),
+                new XElement("FNMECHNGE", fields[10]),
+                new XElement("GRADSTATUS", fields[11]),
+                new XElement("INTTEL", fields[12]),
+                new XElement("INTTEL2", fields[13]),
+                new XElement("INTTEL3", fields[14]),
+                new XElement("INTTEL4", fields[15]),
+                new XElement("INTTEL5", fields[16]),
+                new XElement("SNAMECHANGE", fields[17]),
+                new XElement("SURNAME", fields[18]),
+                new XElement("UKTEL", fields[19]),
+                new XElement("UKTEL2", fields[20]),
+                new XElement("UKTEL3", fields[21]),
+                new XElement("UKTEL4", fields[22]),
+                new XElement("UKTEL5", fields[23]),
+                new XElement("UKMOB", fields[24]),
+                new XElement("UKMOB2", fields[25]),
+                new XElement("UKMOB3", fields[26]),
+                new XElement("UKMOB4", fields[27]),
+                new XElement("UKMOB5", fields[28]))
+                )
+                
 
-                    )
                 );
 
-                return Content(cust.ToString());
+            return Content(Record.ToString());
         }
+        /*Old*/
+        //public IActionResult Convert()
+        //{
+        //    var webroot = _env.WebRootPath;
+        //    var filepath = TempData["filepath"].ToString();
+            
+        //    string[] reader = System.IO.File.ReadAllLines(Path.Combine(webroot, filepath));
+        //    XElement cust = new XElement("Root",
+        //        from str in reader
+        //        let fields = str.Split(',')
+        //        select new XElement("Student",
+        //        new XAttribute("HUSID", fields[0]),
+        //        new XElement("OWNSTU", fields[1]),
+        //        new XElement("Country", fields[2]),
+        //        new XElement("GOForenames", fields[3]),
+        //        new XElement("GOSurname", fields[4]),
+        //        new XElement("IsisForename", fields[5]),
+        //        new XElement("IsisSurname", fields[6]),
+        //        new XElement("FNameChange", fields[7]),
+        //        new XElement("SNameChange", fields[8]),
+        //        new XElement("GradStatus", fields[9]),
+        //        new XElement("UKMob", fields[10]),
+        //        new XElement("UKMob2", fields[11]),
+        //        new XElement("UKTel", fields[12]),
+        //        new XElement("UKTel2", fields[13]),
+        //        new XElement("IntTel", fields[14]),
+        //        new XElement("InteTel2", fields[15]),
+        //        new XElement("Email", fields[16]),
+        //        new XElement("Email2", fields[17]),
+        //        new XElement("PersonUrn", fields[18]),
+        //        new XElement("TblMasterFacultyCode", fields[19]),
+        //        new XElement("PrimaryDept", fields[20]),
+        //        new XElement("StudentNumber", fields[21]),
+        //        new XElement("TblAddStuDetailsCode", fields[22]),
+        //        new XElement("PrimaryTargetCode", fields[23]),
+        //        new XElement("PrimaryTargetName", fields[24]),
+        //        new XElement("IncomeStatusCode", fields[25]),
+        //        new XElement("EnrolmentStatusCode", fields[26]),
+        //        new XElement("MobileNumber", fields[27]),
+        //        new XElement("PersonalEmail", fields[28]),
+        //        new XElement("EmailAddress", fields[29]),
+        //        new XElement("TypeCode", fields[30]),
+        //        new XElement("TelephoneNumber", fields[31]),
+        //        new XElement("HesaReasonForLeavingCode", fields[32]),
+        //        new XElement("DateOfWithdrawal", fields[33]),
+        //        new XElement("HFormattedTelNumber", fields[34]),
+        //        new XElement("MatchMobileAndTelNo", fields[35])
+
+        //            )
+        //        );
+
+        //        return Content(cust.ToString());
+        //}
 
         //private async Task WriteToFileAsAsync()
         //{
